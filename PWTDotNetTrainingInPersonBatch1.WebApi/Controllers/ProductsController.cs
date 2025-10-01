dfcf8ab91b8bec354862fdcadd6e8a53cbe04726
@OutKast317 <phonewinttun173@gmail.com>
2025-10-01 22:25:26 +0630

    Add project files.

---
+++ PWTDotNetTrainingInPersonBatch1.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace PWTDotNetTrainingInPersonBatch1.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly SqlConnectionStringBuilder _stringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "InPersonBatch1MiniPOS",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+
+
+        public class ProductDto()
+        {
+            public string ProductID { get; set; }
+
+            public string ProductCode { get; set; }
+
+            public string ProductName { get; set; }
+
+            public decimal Price { get; set; }
+
+            public int Quantity { get; set; }
+
+            public bool DeleteFlag { get; set; }
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+
+            using (IDbConnection db = new SqlConnection(_stringBuilder.ConnectionString))
+            {
+                db.Open();
+
+                string query = @"SELECT [ProductID]
+                                      ,[ProductCode]
+                                      ,[ProductName]
+                                      ,[Price]
+                                      ,[Quantity]
+                                      ,[DeleteFlag]
+                                  FROM [dbo].[tbl_Product]";
+
+                var lst = db.Query<ProductDto>(query).ToList();
+
+
+                return Ok(lst);
+
+            }
+
+            
+        }
+
+    }
+}
