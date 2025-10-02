da938c64a660205a8b025ff29a779c765f263576
@OutKast317 <phonewinttun173@gmail.com>
2025-10-02 19:42:08 +0630

    feat: updateProduct

--- PWTDotNetTrainingInPersonBatch1.WebApi/Controllers/ProductsController.cs
+++ PWTDotNetTrainingInPersonBatch1.WebApi/Controllers/ProductsController.cs
@@ -24,7 +24,7 @@ namespace PWTDotNetTrainingInPersonBatch1.WebApi.Controllers
 
         public class ProductDto()
         {
-            public string ProductID { get; set; }
+            public string? ProductID { get; set; }
 
             public string ProductCode { get; set; }
 
@@ -42,6 +42,8 @@ namespace PWTDotNetTrainingInPersonBatch1.WebApi.Controllers
             public bool IsSuccess { get; set; }
 
             public String Message { get; set; }
+
+            public ProductDto? Data { get; set; }
         }
 
         [HttpGet]
@@ -69,9 +71,46 @@ namespace PWTDotNetTrainingInPersonBatch1.WebApi.Controllers
         }
 
 
+        [HttpGet("id/{id}")]
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(string id)
+        {
+
+            using (IDbConnection db = new SqlConnection(_stringBuilder.ConnectionString))
+            {
+                db.Open();
+
+                string query = @"SELECT [ProductID]
+                                      ,[ProductCode]
+                                      ,[ProductName]
+                                      ,[Price]
+                                      ,[Quantity]
+                                      ,[DeleteFlag]
+                                  FROM [dbo].[tbl_Product] Where ProductId = @ProductId";
+
+                var item = db.Query<ProductDto>(query, new { ProductId = id }).FirstOrDefault();
+
+                if (item is null)
+                {
+                    return NotFound(new ProductResponseDto()
+                    {
+                        IsSuccess = false,
+                        Message = "Product not found."
+                    });
+                }
+
+                return Ok(new ProductResponseDto
+                {
+                    IsSuccess = true,
+                    Message = "Product found.",
+                    Data = item
+                });
+            }
+        }
+
 
         [HttpPost]
-        public IActionResult CreateProduct(ProductDto request)
+        public IActionResult CreateProduct([FromBody] ProductDto request)
         {
             using (IDbConnection db = new SqlConnection(_stringBuilder.ConnectionString))
             {
@@ -79,22 +118,22 @@ namespace PWTDotNetTrainingInPersonBatch1.WebApi.Controllers
 
                 request.ProductID = Guid.NewGuid().ToString();
 
-                string query = @"INSERT INTO [dbo].[Tbl_Product]
-                                       ([ProductId]
+                string createQuery = @"INSERT INTO [dbo].[tbl_Product]
+                                       ([ProductID]
                                        ,[ProductCode]
                                        ,[ProductName]
                                        ,[Price]
                                        ,[Quantity]
                                        ,[DeleteFlag])
                                  VALUES
-                                       (@ProductId
+                                       (@ProductID
                                        ,@ProductCode
                                        ,@ProductName
                                        ,@Price
                                        ,@Quantity
                                        ,@DeleteFlag)";
                  
-                var result = db.Execute(query, request);
+                var result = db.Execute(createQuery, request);
                 string message = result > 0 ? "Product created successfully." : "Failed to create product.";
                 
                 ProductResponseDto response = new ProductResponseDto()
@@ -106,5 +145,98 @@ namespace PWTDotNetTrainingInPersonBatch1.WebApi.Controllers
                 return Ok(response);
             }
         }
-    }
+
+        [HttpPatch("{id}")]
+        public IActionResult UpdateProduct(string id, [FromBody] ProductDto request)
+        {
+            using (IDbConnection db = new SqlConnection(_stringBuilder.ConnectionString))
+            {
+                db.Open();
+
+
+                //[ProductID]
+                //,[ProductCode]
+                //,[ProductName]
+                //,[Price]
+                //,[Quantity]
+
+                //[ProductCode] = @ProductCode
+                //  ,[ProductName] = @ProductName
+                //  ,[Price] = @Price
+                //  ,[Quantity] = @Quantity[ProductCode] = @ProductCode
+                //  ,[ProductName] = @ProductName
+                //  ,[Price] = @Price
+                //  ,[Quantity] = @Quantity
+
+
+                string conditions = "";
+                if (!string.IsNullOrEmpty(request.ProductCode))
+                {
+                    conditions += "[ProductCode] = @ProductCode, ";
+                }
+                if (!string.IsNullOrEmpty(request.ProductName))
+                {
+                    conditions += "[ProductName] = @ProductName, ";
+                }
+                if (request.Price > 0)
+                {
+                    conditions += "[Price] = @Price, ";
+                }
+                if (request.Quantity > 0)
+                {
+                    conditions += "[Quantity] = @Quantity, ";
+                }
+
+                if (conditions.Length == 0)
+                {
+                    return BadRequest(new ProductResponseDto
+                    {
+                        IsSuccess = false,
+                        Message = "Invalid. Please fill in data to update."
+                    });
+                }
+                
+                conditions = conditions.Substring(0, conditions.Length - 2);
+
+                
+                string updateQuery = $@"UPDATE [dbo].[tbl_Product]
+                                        SET {conditions}
+                                        WHERE [ProductID] = @ProductID";
+
+                request.ProductID = id;
+
+                var result = db.Execute(updateQuery, request);
+                string message = result > 0 ? "Update Successful." : "Update Failed.";
+                return Ok(new ProductResponseDto
+                {
+                    IsSuccess = true,
+                    Message = "Success."
+                });
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 }
